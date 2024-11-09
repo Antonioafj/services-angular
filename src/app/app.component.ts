24efcfc7a2fc835becc64b470fa95fec2ee300c8
Antonioafj <antonioafj.edu@gmail.com>
2024-11-09 16:49:29 -0400

    buscador de pokemon utilizando API do pokeApi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from "./components/card/card.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, CardComponent, CommonModule],
+  template: `
+  <h1>Aula De Services</h1>
+  <app-card></app-card>
+  <router-outlet />
+  
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
